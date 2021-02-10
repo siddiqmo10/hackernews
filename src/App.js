dbf917fa8746196bc116aefa1ad36099800e2580
hisham <hishamsid5@gmail.com>
2021-02-10 16:38:45 -0500

    added complex object in render

--- src/App.js
+++ src/App.js
@@ -4,9 +4,14 @@ import "./App.css";
 class App extends Component {
   render() {
     var helloWorld = "Welcome to the Road to learn React!";
+    var user = { firstName: "mo", lastName: "sid" };
+
     return (
       <div className="App">
         <h2>{helloWorld}</h2>
+        <h3>
+          My name is {user.firstName} {user.lastName}{" "}
+        </h3>
       </div>
     );
   }
