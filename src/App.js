13fa544e53782f11b8c1ff314d1ecb830ed3607f
hisham <hishamsid5@gmail.com>
2021-02-10 16:31:43 -0500

    changed to class

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React, { Component } from "react";
 import "./App.css";
-import { render } from "react-dom";
 
-function App() {
-  return (
-    <div className="App">
-      <h2>Welcome to the Road to learn React!</h2>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h2>Welcome to the Road to learn React!</h2>
+      </div>
+    );
+  }
 }
 
 export default App;
