b6f060db1369676ae566c9032f3135a2bff31a82
hisham <hishamsid5@gmail.com>
2021-02-10 16:29:09 -0500

    init

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import { render } from "react-dom";
 function App() {
   return (
     <div className="App">
-      <h2>Welcome to the Road to learn React</h2>
+      <h2>Welcome to the Road to learn React!</h2>
     </div>
   );
 }
