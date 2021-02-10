a87c8705cd4799e1f315ebd0eafe0cfb727c7af0
hisham <hishamsid5@gmail.com>
2021-02-10 16:24:25 -0500

    init

--- src/App.js
+++ src/App.js
@@ -1,15 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
-import { render } from 'react-dom';
+import React, { Component } from "react";
+import "./App.css";
+import { render } from "react-dom";
 
 function App() {
-  render() {
   return (
     <div className="App">
       <h2>Welcome to the Road to learn React</h2>
     </div>
   );
-}
 }
 
 export default App;
